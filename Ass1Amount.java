59118191578aca2b12942156d89e0b5627617ef1
MrInterBugs <Aedan@mrinterbugs.uk>
2019-10-22 16:09:09 +0100

    Added comments and cleaned up code.

--- Ass1Amount.java
+++ Ass1Amount.java
@@ -6,21 +6,21 @@ import java.lang.reflect.Array;
 
 public class Ass1Amount {
 	public static void main(String[] args) throws FileNotFoundException {
-		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-		int[] amount = new int[27];
-		int len = 0;
+		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray(); //Creating an array with the alphabet inside.
+		int[] amount = new int[27]; //Creating an array full of 27 blank spaces for counting the alphabet and puntuation marks.
+		int len = 0; //The total length of the file which has been imported.
 		int i = 0;
 		int j = 0;
-		File txt = new File("jc.txt");
-		String full = "";
+		File txt = new File("jc.txt"); //Importing the text file that we are working with.
+		String full = ""; //Blank string to import the .txt file into.
 		int count = 0;
 		Scanner scan = new Scanner(txt);
 		while(scan.hasNextLine()) {
 			String line = scan.nextLine();
 			full = full + line;
 		}
-		full = full.replaceAll("\\s+","");
-		full = full.toLowerCase();
+		full = full.replaceAll("\\s+",""); //Removing spaces from the imported text file.
+		full = full.toLowerCase(); //Making the whole import one case so it is easier to work with.
 		len=full.length();
 		for(j=0; j<26; j++) {
 			for(i=0; i<len; i++) {
